bb204796ec679d33a7b9dff44421b56afec03c06
Mahesh Kulkarni <kulkarnimaheshkumar222@gmail.com>
2026-06-14 11:23:26 +0530

    complete assignment 1 and 2 of 13 June 2026 class

---
+++ Assignment/Assignment_1_2/assignment_1_2.py
@@ -0,0 +1,26 @@
+# Assignment 1 and 2 of 13 June 2026 class
+
+# Write Program that accept two number from user add print 
+# Addition , Substraction, Multiplication, Division
+
+firstNumber = int(input("Enter first number"))
+secondNumber = int(input("Enter second number"))
+
+addition = firstNumber + secondNumber
+print("Addition: ", addition)
+
+substration = firstNumber - secondNumber
+print("Substraction: ", substration)
+
+multiplication = firstNumber * secondNumber
+print("Multiplication: ", multiplication)
+
+division = firstNumber // secondNumber
+print("Division: ", division)
+
+# Write program to take username and age and display
+
+name = input("Enter your name")
+age = int(input("Enter your age"))
+
+print(f"Hello {name}, you will turn {age+1} next year.")
