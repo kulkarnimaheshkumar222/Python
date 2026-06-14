a355b68ac36763011c1d597319d645587a702b76
Mahesh Kulkarni <kulkarnimaheshkumar222@gmail.com>
2026-06-14 19:58:14 +0530

    add code for 13 June 2026

---
+++ Classroom /13June26/TypeDemo.py
@@ -0,0 +1,15 @@
+no = 11 # Int
+marks = 90.78  # float
+value = 8+5j # complex
+
+print(type(no))
+print(type(marks))
+print(type(value))
+
+gender = True # bool
+print(gender)
+print(type(gender))
+
+demo = None
+print(demo)
+print(type(demo))
