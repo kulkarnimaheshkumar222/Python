7ce6510bec092c9cd98785e659fd9b0ac18091c4
Mahesh Kulkarni <kulkarnimaheshkumar222@gmail.com>
2026-06-14 19:52:52 +0530

    add code for 14 June 2026

---
+++ Classroom /14June2026/IfElseDemo.py
@@ -0,0 +1,6 @@
+age = int(input("Enter your age"))
+
+if (age > 18):
+    print("You are allowed to enter")
+else:
+    print("Sorry !! You are not allowed to enter")
---
+++ Classroom /14June2026/tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+("Free entry")
+elif ((age > 5) and (age <= 18)):
+    print("Entry fees is 900")
+    entryFees = 900
+elif ((age > 18) and (age <= 40)):
+    print("Entry fees is 1200")
+else :
+    print("Entry fees is 500")
